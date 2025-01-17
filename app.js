1381edc2e9ca5f494ea28880109b33ea888539de
Jayson Bartels <jrbartels24@gmail.com>
2025-01-17 13:35:42 -0800

    Added time feature

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ function writeDate() {
         weekday: 'long',
         month: 'long',
         day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
         hour12: true
     });
 
